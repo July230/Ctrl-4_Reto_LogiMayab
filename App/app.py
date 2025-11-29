5eb415af038e511a0d0f3454e02355277b8f2c25
July230 <A01352823@tec.mx>
2025-11-29 01:02:16 -0600

    Cambiar nombre de funcion

--- App/app.py
+++ App/app.py
@@ -1,12 +1,12 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 from views.layout_main import layout
-from controllers.callbacks_upload import register_callbacks
+from controllers.callbacks_upload import register_callbacks_upload
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = layout
 
-register_callbacks(app)
+register_callbacks_upload(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- App/controllers/callbacks_upload.py
+++ App/controllers/callbacks_upload.py
@@ -2,13 +2,13 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from models.file_loader import load_upload_file
 
-def register_callbacks(app):
+def register_callbacks_upload(app):
     @app.callback(
         Output('uploaded-file-info', 'children'),
         Input('upload-data', 'contents'),
         State('upload-data', 'contents'),
     )
-    def handle_upload(contents, filename):
+    def load_file(contents, filename):
         if contents:
             df = load_upload_file(contents, filename)
             return f'Archivo cargado: {filename} {len(df)} filas'
