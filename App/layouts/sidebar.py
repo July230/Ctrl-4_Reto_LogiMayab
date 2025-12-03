f0c09e9fc2854fb09ee2e7fd868f3628ca59801a
July230 <A01352823@tec.mx>
2025-12-03 16:24:56 -0600

    Hacer que barra lateral se oculte al pasar el cursor

--- App/layouts/sidebar.py
+++ App/layouts/sidebar.py
@@ -8,20 +8,12 @@ sidebar = html.Div(
         html.P("A simple sidebar layout with navigation links", className="lead"),
         dbc.Nav(
             [
-                dbc.NavLink('Dashboard', href='/dashboard', active='exact'),
-                dbc.NavLink('Otra Página', href='/otra-pagina', active='exact'),
+                dbc.NavLink('Dashboard', href='/dashboard', active='exact', className='nav-link'),
+                dbc.NavLink('Otra Página', href='/otra-pagina', active='exact', className='nav-link'),
             ],
             vertical=True,
             pills=True,
         ),
     ],
-    style={
-        "width": "18rem",
-        "padding": "2rem",
-        "background-color": "#f8f9fa",
-        "height": "100vh",
-        "position": "fixed",
-        "top": 0,
-        "left": 0
-    }
+    className='sidebar'
 )
