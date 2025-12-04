8f1cdf149af334ea659a4d0c22826f93835b94c9
July230 <A01352823@tec.mx>
2025-12-04 12:20:32 -0600

    Crear funcion plot_routes

---
+++ App/callbacks/callbacks_dashboard/plots/plot_routes.py
@@ -0,0 +1,36 @@
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from empty_fig import empty_fig
+
+def plot_frequent_routes(df):
+    '''
+    Genera la gráfica de las top 10 rutas más frecuentes.
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame con los datos cargados y limpios.
+    Returns
+    -------
+    plotly.graph_objects.Figure
+        Gráfica de barras horizontales con las 10 rutas más frecuentes.
+    '''
+    if 'Ruta' not in df.columns:
+        return empty_fig('La columna "Ruta" no existe en los datos.')
+
+    top_routes = df['Ruta'].value_counts().nlargest(10).reset_index()
+    top_routes.columns = ['Ruta', 'Frecuencia']
+    top_routes = top_routes.sort_values('Frecuencia', ascending=True)
+
+    fig = px.bar(
+        top_routes,
+        x='Frecuencia',
+        y='Ruta',
+        orientation='h',
+        title='Top 10 rutas más frecuentes'
+    )
+    fig.update_layout(yaxis={'categoryorder': 'total ascending'})
+
+    return fig
