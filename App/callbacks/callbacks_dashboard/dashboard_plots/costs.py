5687a8989c2d9ffa120d89dc8b489dec6c1d2c03
a01709519 <A01709519@tec.mx>
2025-12-05 14:44:38 -0600

    Agregar funcion para generar graficas

---
+++ App/callbacks/callbacks_dashboard/dashboard_plots/costs.py
@@ -0,0 +1,18 @@
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import plotly.express as px
+from utils.empty_fig import empty_fig
+import numpy as np
+import pandas as pd
+
+
+
+def plot_costs(df):
+    '''
+    
+
+                    
+    '''
+    fig=px.bar(df, x='Nombre_Cliente', y='CV_Mensual')
+
+    return fig
