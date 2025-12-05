6d9d08eb3cba6d36e2b3ef22cae41a8f74ef1aec
July230 <A01352823@tec.mx>
2025-12-04 19:32:49 -0600

    Poner graficas en tarjetas

--- App/layouts/dashboard.py
+++ App/layouts/dashboard.py
@@ -35,17 +35,29 @@ dashboard_content = dbc.Container([
 
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot-1', className='dashboard-graph')
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='plot-1', className='dashboard-graph')
+                ])
+            ], className='shadow-sm mb-3', style={'minHeight': '300px'})
         ], width=6),
         dbc.Col([
-            dcc.Graph(id='plot-2', className='dashboard-graph')
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='plot-2', className='dashboard-graph')
+                ])
+            ], className='shadow-sm mb-3', style={'minHeight': '300px'})
         ], width=6)
     ]),
 
 
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot-3', className='dashboard-graph')
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='plot-3', className='dashboard-graph')
+                ])
+            ], className='shadow-sm mb-3', style={'minHeight': '300px'})
         ], width=6),
         #dbc.Col([
             # Placeholder for a fourth plot to complete a 2x2 grid. Add a callback for 'plot-4' when available.
