1b5a93af522737f6a36a4990bdd52057716dcdd1
July230 <A01352823@tec.mx>
2025-12-04 18:48:32 -0600

    Graficos en disposicion 2x2 en dashboard

--- App/layouts/dashboard.py
+++ App/layouts/dashboard.py
@@ -32,19 +32,21 @@ dashboard_content = dbc.Container([
 
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot-1')
-        ], width=12)
+            dcc.Graph(id='plot-1', className='dashboard-graph')
+        ], width=6),
+        dbc.Col([
+            dcc.Graph(id='plot-2', className='dashboard-graph')
+        ], width=6)
     ]),
+
 
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot-2')
-        ], width=12)
-    ]),
-
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='plot-3')
-        ], width=12)
+            dcc.Graph(id='plot-3', className='dashboard-graph')
+        ], width=6),
+        #dbc.Col([
+            # Placeholder for a fourth plot to complete a 2x2 grid. Add a callback for 'plot-4' when available.
+        #    dcc.Graph(id='plot-4', className='dashboard-graph')
+        #], width=6)
     ])
 ])
